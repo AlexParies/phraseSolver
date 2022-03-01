a16418597089fb46eca5f7f898adb339989abd94
AlexParies <alexandertpries@gmail.com>
2022-03-01 12:42:04 -0600

    step 4

--- PhraseSolver.java
+++ PhraseSolver.java
@@ -4,15 +4,18 @@
  *  The PhraseSolver class the PhraseSolverGame
  */
 import java.util.Scanner;
-  
+
 public class PhraseSolver
 {
   /* your code here - attributes */
-
-  /* your code here - constructor(s) */ 
+  private Player player1;
+  private Player player2;
+  private Board game;
+  private boolean solved;
+  /* your code here - constructor(s) */
 
   /* your code here - accessor(s) */
-  
+
   /* your code here - mutator(s)  */
 
   public void play()
@@ -21,18 +24,18 @@ public class PhraseSolver
     int currentPlayer = 1;
 
     Scanner input = new Scanner(System.in);
-    
+
     boolean correct = true;
-    while (!solved) 
+    while (!solved)
     {
-      
+
       /* your code here - game logic */
-      
-      
+
+
       /* your code here - determine how game ends */
-      solved = true; 
-    } 
-   
+      solved = true;
+    }
+
   }
-  
+
 }
--- Player.java
+++ Player.java
@@ -1,6 +1,6 @@
 /*
  * Activity 2.5.2
- * 
+ *
  * A Player class the PhraseSolverGame
  */
 import java.util.Scanner;
@@ -8,10 +8,11 @@ import java.util.Scanner;
 public class Player
 {
   /* your code here - attributes */
+  private String name;
+  private int score;
+  /* your code here - constructor(s) */
 
-  /* your code here - constructor(s) */ 
-
-  /* your code here - accessor(s) */ 
+  /* your code here - accessor(s) */
 
   /* your code here - mutator(s) */ 
 }
